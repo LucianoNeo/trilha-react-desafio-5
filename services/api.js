d536004521dfd8b6c120116af99a3247006cccee
Luciano Bueno <luciano.bueno@ingacode.com.br>
2024-11-01 18:23:50 -0300

    feat: entrega solução do desafio

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+    baseURL: 'https://gcatqfekpsqoordijxfm.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdjYXRxZmVrcHNxb29yZGlqeGZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA0ODM1OTAsImV4cCI6MjA0NjA1OTU5MH0.UAIZS5wwhPI5PGvENI-HA3lDxDZlAwnz2HsmjPF1Yg8",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdjYXRxZmVrcHNxb29yZGlqeGZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA0ODM1OTAsImV4cCI6MjA0NjA1OTU5MH0.UAIZS5wwhPI5PGvENI-HA3lDxDZlAwnz2HsmjPF1Yg8"
     }
 })
